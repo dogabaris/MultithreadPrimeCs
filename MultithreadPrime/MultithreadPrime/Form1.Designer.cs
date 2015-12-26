7c1d42a9b4b2da2cb1b07301fd5cdf1461a821e5
Doğa Barış <dogabarisozdemir@gmail.com>
2015-12-26 05:19:11 +0200

    init

---
+++ MultithreadPrime/MultithreadPrime/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace MultithreadPrime
+{
+    partial class MultiThreadPrime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database2DataSet = new MultithreadPrime.Database2DataSet();
+            this.label_sayi = new System.Windows.Forms.Label();
+            this.label_arastirilan = new System.Windows.Forms.Label();
+            this.btn_bitir = new System.Windows.Forms.Button();
+            this.btn_basla = new System.Windows.Forms.Button();
+            this.label_snc = new System.Windows.Forms.Label();
+            this.label_sonuc = new System.Windows.Forms.Label();
+            this.tableTableAdapter = new MultithreadPrime.Database2DataSetTableAdapters.TableTableAdapter();
+            this.tableAdapterManager = new MultithreadPrime.Database2DataSetTableAdapters.TableAdapterManager();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Thread = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Baslangic = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Bitis = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Asalmi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.database2DataSet;
+            // 
+            // database2DataSet
+            // 
+            this.database2DataSet.DataSetName = "Database2DataSet";
+            this.database2DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label_sayi
+            // 
+            this.label_sayi.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_sayi.AutoSize = true;
+            this.label_sayi.Location = new System.Drawing.Point(1005, 130);
+            this.label_sayi.Name = "label_sayi";
+            this.label_sayi.Size = new System.Drawing.Size(13, 13);
+            this.label_sayi.TabIndex = 8;
+            this.label_sayi.Text = "0";
+            // 
+            // label_arastirilan
+            // 
+            this.label_arastirilan.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_arastirilan.AutoSize = true;
+            this.label_arastirilan.Location = new System.Drawing.Point(911, 130);
+            this.label_arastirilan.Name = "label_arastirilan";
+            this.label_arastirilan.Size = new System.Drawing.Size(81, 13);
+            this.label_arastirilan.TabIndex = 7;
+            this.label_arastirilan.Text = "Araştırılan Sayı :";
+            // 
+            // btn_bitir
+            // 
+            this.btn_bitir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_bitir.Location = new System.Drawing.Point(1005, 80);
+            this.btn_bitir.Name = "btn_bitir";
+            this.btn_bitir.Size = new System.Drawing.Size(75, 23);
+            this.btn_bitir.TabIndex = 6;
+            this.btn_bitir.Text = "Bitir";
+            this.btn_bitir.UseVisualStyleBackColor = true;
+            this.btn_bitir.Click += new System.EventHandler(this.btn_bitir_Click);
+            // 
+            // btn_basla
+            // 
+            this.btn_basla.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_basla.Location = new System.Drawing.Point(914, 80);
+            this.btn_basla.Name = "btn_basla";
+            this.btn_basla.Size = new System.Drawing.Size(75, 23);
+            this.btn_basla.TabIndex = 5;
+            this.btn_basla.Text = "Başla";
+            this.btn_basla.UseVisualStyleBackColor = true;
+            this.btn_basla.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // label_snc
+            // 
+            this.label_snc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_snc.AutoSize = true;
+            this.label_snc.Location = new System.Drawing.Point(911, 177);
+            this.label_snc.Name = "label_snc";
+            this.label_snc.Size = new System.Drawing.Size(44, 13);
+            this.label_snc.TabIndex = 10;
+            this.label_snc.Text = "Sonuç :";
+            // 
+            // label_sonuc
+            // 
+            this.label_sonuc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label_sonuc.AutoSize = true;
+            this.label_sonuc.Location = new System.Drawing.Point(914, 211);
+            this.label_sonuc.Name = "label_sonuc";
+            this.label_sonuc.Size = new System.Drawing.Size(103, 13);
+            this.label_sonuc.TabIndex = 11;
+            this.label_sonuc.Text = "Sayı daha girilmedi...";
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.TableTableAdapter = this.tableTableAdapter;
+            this.tableAdapterManager.UpdateOrder = MultithreadPrime.Database2DataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.AllowUserToResizeColumns = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Thread,
+            this.Baslangic,
+            this.Bitis,
+            this.Asalmi});
+            this.dataGridView.Cursor = System.Windows.Forms.Cursors.No;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.ReadOnly = true;
+            this.dataGridView.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView.Size = new System.Drawing.Size(905, 597);
+            this.dataGridView.TabIndex = 15;
+            // 
+            // Thread
+            // 
+            this.Thread.HeaderText = "Thread";
+            this.Thread.Name = "Thread";
+            this.Thread.ReadOnly = true;
+            // 
+            // Baslangic
+            // 
+            this.Baslangic.HeaderText = "Bas.Nok";
+            this.Baslangic.Name = "Baslangic";
+            this.Baslangic.ReadOnly = true;
+            // 
+            // Bitis
+            // 
+            this.Bitis.HeaderText = "Bit.Nok";
+            this.Bitis.Name = "Bitis";
+            this.Bitis.ReadOnly = true;
+            // 
+            // Asalmi
+            // 
+            this.Asalmi.HeaderText = "Asallık";
+            this.Asalmi.Name = "Asalmi";
+            this.Asalmi.ReadOnly = true;
+            // 
+            // MultiThreadPrime
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 595);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.label_sonuc);
+            this.Controls.Add(this.label_snc);
+            this.Controls.Add(this.label_sayi);
+            this.Controls.Add(this.label_arastirilan);
+            this.Controls.Add(this.btn_bitir);
+            this.Controls.Add(this.btn_basla);
+            this.Name = "MultiThreadPrime";
+            this.Text = "MultiThreadus Prime";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database2DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_sayi;
+        private System.Windows.Forms.Label label_arastirilan;
+        private System.Windows.Forms.Button btn_bitir;
+        private System.Windows.Forms.Button btn_basla;
+        private System.Windows.Forms.Label label_snc;
+        private System.Windows.Forms.Label label_sonuc;
+        private Database2DataSet database2DataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private Database2DataSetTableAdapters.TableTableAdapter tableTableAdapter;
+        private Database2DataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Thread;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Baslangic;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Bitis;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Asalmi;
+    }
+}
+
---
+++ MultithreadPrime/MultithreadPrime/Form1.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MultithreadPrime
+{
+    public partial class MultiThreadPrime : Form
+    {
+        bool flag=true;
+        int karekoksayi, aralik, kalan,suankisayi,threadnumarasi;
+        int threadsayisi,asallik,elemansayisi,sonasalsayi;
+        MySqlConnection baglanti;
+        List<string> sonuc = new List<string>();
+        List<string> arasonuc = new List<string>();
+        List<List<int>> limitler = new List<List<int>>();
+        
+
+        public MultiThreadPrime()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //btn_bitir.Enabled = false;
+            MySqlConnectionStringBuilder build = new MySqlConnectionStringBuilder();
+            string bag;
+
+            build.UserID = "root";
+            build.Password = "";
+            build.Database = "multithreadprime";
+            build.Server = "localhost";
+
+            bag = build.ToString();
+
+            baglanti = new MySqlConnection(bag);
+            SonuncuyuGetir();
+            
+            this.dataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridView.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridView.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            this.dataGridView.Columns[3].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+
+        }
+
+        private void btn_basla_Click(object sender, EventArgs e)
+        {
+            flag = true;
+            btn_basla.Hide();
+            Thread BaslaThreeadi = new Thread(() => BaslaButonu());
+            BaslaThreeadi.Start();
+            
+        }
+
+        private void num_sayi_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void BaslaButonu()
+        {
+            while (flag)
+            {
+            label_sayi.Text = sonasalsayi.ToString();
+            sonuc.Clear();
+            limitler.Clear();
+            List<int> asalsayilistesi = new List<int>();
+            karekoksayi = (int)Math.Sqrt(Convert.ToDouble(sonasalsayi));
+
+            for (int i = 2; i <= karekoksayi; i++)
+            {
+                asallik = 1;
+                for (int k = 2; k < i; k++)
+                {
+
+                    if (i % k == 0)//asal degildir.
+                    {
+                        asallik = 0;
+
+                        break;
+                    }
+                }
+
+                if (asallik == 1)
+                    asalsayilistesi.Add(i);//asaldir.
+
+            }
+
+            elemansayisi = asalsayilistesi.Count;
+            threadsayisi = (elemansayisi / 100) + 1;
+
+            if (elemansayisi <= 100)
+                threadsayisi = 2;
+
+            aralik = asalsayilistesi.Count / threadsayisi;
+            kalan = asalsayilistesi.Count % threadsayisi;
+
+            int sayac = 0;
+            int baslangic = 0;
+
+            for (int j = 0; j < threadsayisi; j++)
+            {
+                List<int> row = new List<int>();
+
+                for (int i = baslangic; i <= asalsayilistesi.Count; i++)
+                {
+                    if (sayac == aralik + kalan)
+                        break;
+
+
+                    try//4,5,6,7,8 sayılarında for döngüsü durmayıp dizinin olmayan bir indexini yeni diziye atamaya çalışıyor bu yüzden try-catch
+                    {
+                        row.Add(asalsayilistesi[i]);
+                        sayac++;
+                    }
+                    catch (System.ArgumentOutOfRangeException)
+                    {
+                        //break;
+                    }
+
+                }
+                sayac = 0;
+                baslangic += aralik + kalan;
+
+                limitler.Add(row);
+            }
+
+            foreach (List<int> row in limitler)
+            {
+                foreach (int r in row)
+                {
+                    Debug.WriteLine(r.ToString());
+                }
+
+                Debug.WriteLine("ara");
+            }
+
+            List<Thread> Threadlar = new List<Thread>();
+            
+
+
+            for (int c = 0; c < threadsayisi; c++)
+            {
+                sonuc.Add("false");
+                int copy = c;
+                Threadlar.Add(new Thread(() => ThreadFonksiyonu(limitler[copy], copy)));    
+            }
+
+
+            foreach (Thread thread in Threadlar)
+            {
+                thread.Start();
+            }
+
+            foreach (Thread thread in Threadlar)
+            {
+                thread.Join();
+            }
+
+            //SqlTablosunuTemizle();
+            //AsalSayilariSqleYaz(asalsayilistesi);
+            //SqlIslemi.Start();
+            //SqlIslemi.Join();
+            SayiAsalMi(sonuc);
+            TabloyuCiz(aralik);
+            sonasalsayi += 2;
+            }
+        }
+        private void SayiAsalMi(List<string> sonuc)
+        {
+            if (sonuc.Contains("true")==false)
+            {
+                Thread SqlIslemi = new Thread(() => AsalSayilariSqleYaz(sonasalsayi));
+                //AsalSayilariSqleYaz(sonasalsayi);
+                SqlIslemi.Start();
+                SqlIslemi.Join();
+            }
+        }
+        private void SonuncuyuGetir()
+        {
+            baglanti.Open();          
+            MySqlCommand getir = new MySqlCommand("SELECT asalsayilar FROM tablo ORDER BY asalsayilar DESC LIMIT 1", baglanti);
+            sonasalsayi = Convert.ToInt32(getir.ExecuteScalar());
+            baglanti.Close();
+        }
+
+        private void ThreadFonksiyonu(List<int> limitler,int kacincithread)
+        {
+            string flag="false";
+            for (int k = 0; k < limitler.Count; k++)
+            {
+                if (sonasalsayi % limitler[k] == 0)
+                {
+                    
+                    Debug.WriteLine("true " + limitler[k]);
+                    flag = "true";
+                    sonuc[kacincithread] = flag;
+                    threadnumarasi = k;
+                    suankisayi = limitler[k];
+                    break;
+                }
+                else
+                {
+                    Debug.WriteLine("false");
+                    flag = "false";          
+                }
+                
+            }
+            
+        }
+
+        private void AsalSayilariSqleYaz(int sonasalsayi)
+        {
+            try
+            {
+                baglanti.Open();
+            }
+            catch(System.InvalidOperationException){
+                baglanti.Close();
+            }
+            
+                MySqlCommand ekle = new MySqlCommand("insert into tablo (AsalSayilar) values ('" + sonasalsayi + "')", baglanti);
+                ekle.ExecuteNonQuery();
+
+            baglanti.Close();
+        }
+
+        private void SqlTablosunuTemizle()
+        {
+            string sql = "DELETE FROM tablo";
+            DataTable dt = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+            MySqlCommand command = new MySqlCommand();
+
+            command.CommandText = sql;
+            command.Connection = baglanti;
+            adapter.SelectCommand = command;
+
+            try
+            {
+                baglanti.Open();
+            }
+            catch (System.InvalidOperationException)
+            {
+                baglanti.Close();
+            }
+            adapter.Fill(dt);
+            baglanti.Close();
+        }
+
+        private void TabloyuCiz(int aralik)
+        {
+            
+            dataGridView.Rows.Clear();
+            for (int k = 0; k<threadsayisi ;k++ )
+            {
+                try
+                {
+                    dataGridView.Rows.Add(k, limitler[k].First(), limitler[k].Last(), sonuc[k]); 
+                }
+                catch (System.InvalidOperationException)//4 5 6 7 8 için 2 threade bölünmesine rağmen 2.bir asalsayı eksikliğinden yazdırılamıyordu.
+                {
+
+                }
+                             
+            }
+            
+        }
+
+        private void btn_bitir_Click(object sender, EventArgs e)
+        {
+            flag = false;
+            btn_basla.Show();
+            
+            int hangithread = sonuc.FindIndex(key => key == "true");
+                if(!sonuc.Contains("true"))
+                    label_sonuc.Text = "Sayı asaldır!";
+                else
+                    label_sonuc.Text = "Sayı asal değilidir! \n" + "Thread " + hangithread + " da bulunan " + suankisayi + "\n sayisina tam bölünmektedir.";
+            
+        }
+
+        
+
+        }
+     
+}
+
